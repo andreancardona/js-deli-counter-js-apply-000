9727c7009a2e200d6f78a85d910b3ad7f8cb84c5
andreancardona@gmail.com <andreancardona@gmail.com>
2017-11-13 17:51:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var takeANumber = function(katzDeliLine, newCustomer) {
     if (katzDeliLine.length > 0);
+     katzDeliLine.push(newCustomer);
      return ('Welcome, ' + newCustomer + '. You are number ' + katzDeliLine.length + ' in line.')
   }
 
